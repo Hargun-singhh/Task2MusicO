748a7929a69e6c1936fa1d562ec351984a426a06
Hargun <hargunsingh14.hs@gmail.com>
2024-05-05 19:32:42 +0530

    jS Updated

--- play.js
+++ play.js
@@ -18,3 +18,46 @@ audioElement.addEventListener('timeupdate', ()=>{
 })
 
 
+MyPB.addEventListener('change', ()=>{
+  audioElement.currentTime = MyPB.value * audioElement.duration /100;
+})
+
+  const volumeRange = document.getElementById('volumeRange');
+  const volumeValue = document.getElementById('volumeValue');
+
+  volumeRange.addEventListener('input', function() {
+    const volume = this.value;
+    volumeValue.textContent = volume;
+    audioElement.volume = volume / 100;
+  });
+
+repeatButton.addEventListener('click' ,function(){
+  isRepeat = !isRepeat;
+  if(isRepeat){
+    audioElement.loop = true;
+    console.log("repeat");
+    repeatButton.style.color = "purple";
+  }else{
+    audioElement.loop = false;
+    console.log('repeat off');
+    repeatButton.style.color = "white";
+  }
+});
+
+document.getElementById('searchInput').addEventListener('input', function() {
+      var searchInput = this.value.toLowerCase();
+      var items = document.querySelectorAll('#songls');
+
+  
+      items.forEach(function(item) {
+          var itemName = item.textContent.toLowerCase();
+  
+          if (itemName.includes(searchInput)) {
+              item.style.display = 'block';
+              
+          } else {
+              item.style.display = 'none';
+          }
+      });
+  });
+  window.onscroll = function() {myFunction()};
