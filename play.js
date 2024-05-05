70cbe364848edb44a6e04ec8e996cb20984cc135
Hargun <hargunsingh14.hs@gmail.com>
2024-05-05 19:33:08 +0530

    jS Completed

--- play.js
+++ play.js
@@ -61,3 +61,15 @@ document.getElementById('searchInput').addEventListener('input', function() {
       });
   });
   window.onscroll = function() {myFunction()};
+
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+  
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
+  
